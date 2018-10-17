9c006e64a15f31acf6e536fb94badbd6d0ae019e
nadment <nadment@gmail.com>
2018-10-17 13:34:36 +0200

    Move AbstractStepDialog

--- src/main/java/org/pentaho/di/ui/trans/steps/cleanse/CleanseDialog.java
+++ src/main/java/org/pentaho/di/ui/trans/steps/cleanse/CleanseDialog.java
@@ -46,7 +46,6 @@ import org.pentaho.di.ui.core.dialog.ErrorDialog;
 import org.pentaho.di.ui.core.widget.ColumnInfo;
 import org.pentaho.di.ui.core.widget.ColumnsResizer;
 import org.pentaho.di.ui.core.widget.TableView;
-import org.pentaho.di.ui.dialog.AbstractStepDialog;
 import org.pentaho.di.ui.trans.step.BaseStepDialog;
 import org.pentaho.di.ui.trans.step.TableItemInsertListener;
 
